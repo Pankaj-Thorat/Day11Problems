8cc9226b42c2a0641c44c2dd73381cc665550bc6
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-05-14 00:38:28 +0530

    [Pankaj] To Get count of number of Duplicate Elements in array

---
+++ Day11Problems/DuplicateElements.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11Problems
+{
+    class DuplicateElements
+    {
+        public static void GetDuplicateElements()
+        {
+            int[] array = { 1, 2, 3, 4, 5, 1, 2, 3, 4, 1 };
+
+            Dictionary<int, int> countDict = new Dictionary<int, int>();
+
+            // Count the occurrences of each element in the array
+            foreach (int num in array)
+            {
+                if (countDict.ContainsKey(num))
+                    countDict[num]++;
+                else
+                    countDict[num] = 1;
+            }
+            // Count the number of duplicate elements
+            int duplicateCount = 0;
+            foreach (int count in countDict.Values)
+            {
+                if (count > 1)
+                    duplicateCount++;
+            }
+            Console.Write("Elements of array: ");
+            foreach (int num in array)
+            {                
+                Console.Write(num + " ");               
+            }
+            Console.WriteLine("\nTotal number of duplicate elements: " + duplicateCount);
+        }
+    }
+}
---
+++ Day11Problems/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //getting input from user to execute specific code
+            Console.WriteLine("Please choose your program");
+            Console.WriteLine("1.Duplicate Elements \n2.Exit");
+            int option = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:DuplicateElements.GetDuplicateElements();
+                    break;
+                default:
+                    Console.WriteLine("Please try again and choose from given options only.");
+                    break;
+            }
+        }
+    }
+}
