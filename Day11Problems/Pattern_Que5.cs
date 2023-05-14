04720a88eb4e3b5da3b6ec3432d2c7c8be6323b7
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-05-14 14:38:38 +0530

    [Pankaj] To get the desired pattern in que 5 of day 11

---
+++ Day11Problems/Pattern_Que5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11Problems
+{
+    class Pattern_Que5
+    {
+        public static void GetPatternQue5()
+        {
+            int i, j;
+            Console.WriteLine("Enter the number of lines you want to print the pattern: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (j = 0; j < n; j++)
+            {
+                for (i = 0; i < n - j; i++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine(("\n"));
+            }
+        }
+
+    }
+}
